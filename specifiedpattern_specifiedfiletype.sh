9f479ef1afe3bcd6812f8439dcb01649967828ca
vaishnavibadam <vittu512@gmail.com>
2017-06-23 18:33:19 +0530

    message

--- specifiedpattern_specifiedfiletype.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [[ $# -ne 2 ]]; #condition for checking the number of arguments
-then               
-	echo "usage:$0 <file type> <pattern>" #defining the usage
-	exit 1
-fi 
-find *.$1|xargs grep $2 #finding the file types and searching a specific pattern
