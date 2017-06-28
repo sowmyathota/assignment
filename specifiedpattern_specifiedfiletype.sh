d663d29435d335dc435b3b177a05c3683e7c43e1
sowmyathota <sowmya.thota50@gmail.com>
2017-06-28 10:56:17 +0530

    added usage

--- specifiedpattern_specifiedfiletype.sh
+++ specifiedpattern_specifiedfiletype.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-
-if [[ $# -ne 2 ]]; #condition for checking the number of arguments
-then               
-	echo "usage:$0 <file type> <pattern>" #defining the usage
+if [[ $# -ne 2 ]];
+then              #checking whether input has 2 arguments 
+	echo "usage:$0 <file type> <pattern> or <file type><'pattern1\|pattern2...>"
 	exit 1
 fi 
-find *.$1|xargs grep $2 #finding the file types and searching a specific pattern
+
+find *.$1|xargs grep $2 
+
